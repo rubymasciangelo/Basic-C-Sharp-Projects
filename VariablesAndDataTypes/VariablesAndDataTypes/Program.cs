400f327120ace758716f7f6290c77ae2f0768c1d
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-13 21:29:26 -0800

    program written

---
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine(yourName + " is a dumb name!");
+            Console.Read();
+        }
+    }
+}
