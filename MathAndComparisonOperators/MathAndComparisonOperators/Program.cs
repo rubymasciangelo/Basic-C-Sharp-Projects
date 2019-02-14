315d4eafc27cfd3dabb8e0283107c7ab96ea977f
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-13 23:38:56 -0800

    addition

---
+++ MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 5 + 10;
+            int otherTotal = 12 + 22;
+            int combined = total + otherTotal;
+            Console.WriteLine(combined);
+            Console.ReadLine();
+        }
+    }
+}
