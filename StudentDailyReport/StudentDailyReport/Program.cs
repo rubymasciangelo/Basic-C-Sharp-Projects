93c683624b5bace90ced83653dfb49ee9a92b825
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-13 23:21:27 -0800

    the tech academy student daily report program created

---
+++ StudentDailyReport/StudentDailyReport/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDailyReport
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you on?");
+            string currentCourse = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string pageResponse = Console.ReadLine();
+            int currentPage = Convert.ToInt16(pageResponse);
+            Console.WriteLine("Do you need help  with anything? Please answer " + '"' + "true" + '"' + " or " + '"' + "false" + '"');
+            //string helpResponse = "true";
+            //bool yesHelp = bool.Parse(helpResponse);
+            string helpResponse = Console.ReadLine();
+            bool helpValue = Convert.ToBoolean(helpResponse);
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string positiveResponse = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedbackResponse = Console.ReadLine();
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
+            Console.Read();
+        }
+    }
+}
