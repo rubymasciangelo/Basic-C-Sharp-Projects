2d37e70ac77b9fcc1bb9c2a66ddc3ae7613f3764
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-14 00:08:02 -0800

    1. *50

---
+++ pickANumberExercise/pickANumberExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pickANumberExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write any number: ");
+            string numResponse = Console.ReadLine();
+            int inputNum = Convert.ToInt32(numResponse);
+            int multiply = inputNum * 50;
+            Console.WriteLine("Your number multiplied by 50 is " + multiply);
+        }
+    }
+}
