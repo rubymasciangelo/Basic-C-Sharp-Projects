ea382b9ce0334c17e36aad3197dd5dd421d3de2e
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-13 21:05:59 -0800

    acme accounting program created

---
+++ acme_accounting/acme_accounting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acme_accounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're " + '"' + "accounting" + '"' + " on you!");
+            Console.Read();
+        }
+    }
+}
