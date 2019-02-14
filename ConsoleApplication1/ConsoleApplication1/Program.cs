3061f83557ab57ee1253a7071ed868f2363793e4
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-13 20:33:54 -0800

    response using the variable added

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
         }
     }
 }
