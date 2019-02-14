4e125e2c3ce3bdf3c25e882f0a009cac3afccae0
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-13 23:40:55 -0800

    subtraction

--- MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
+++ MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
@@ -10,10 +10,14 @@ namespace MathAndComparisonOperators
     {
         static void Main(string[] args)
         {
-            int total = 5 + 10;
-            int otherTotal = 12 + 22;
-            int combined = total + otherTotal;
-            Console.WriteLine(combined);
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            int difference = 10 - 5;
+            Console.WriteLine("math " + difference.ToString());
             Console.ReadLine();
         }
     }
