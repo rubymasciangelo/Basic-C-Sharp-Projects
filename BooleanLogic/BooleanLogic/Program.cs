00ff93b771b810561ec1371a19dcd7a2a3d3ba2a
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-15 02:01:37 -0800

    'does not equal' operator

--- BooleanLogic/BooleanLogic/Program.cs
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -21,6 +21,10 @@ namespace BooleanLogic
             Console.WriteLine(true == true);
             Console.WriteLine(true == false);
             Console.WriteLine(false == false);
+
+            Console.WriteLine(true != true);
+            Console.WriteLine(true != false);
+            Console.WriteLine(false != false);
         }
     }
 }
