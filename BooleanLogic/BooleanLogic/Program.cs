3ff32a301c4050e636186e1361e0084f83fbf822
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-15 01:57:44 -0800

    boolean 'and' operator

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(true && false);
+            Console.WriteLine(true && true);
+            Console.WriteLine(false && false);
+
+        }
+    }
+}
