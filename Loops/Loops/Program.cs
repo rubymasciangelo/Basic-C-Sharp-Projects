db1e194335eb636bd929b8a9d641c8d964b4ef67
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-16 03:31:17 -0800

    do while loops and while loops and switch

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("guess a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+
+            do
+            {
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("no not 62");
+                        Console.WriteLine("guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("no not 29");
+                        Console.WriteLine("guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("no not 55");
+                        Console.WriteLine("guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("wow yes 12");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("no! wrong!");
+                        Console.WriteLine("guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+
+            while (!isGuessed);
+            Console.Read();
+        }
+    }
+}
