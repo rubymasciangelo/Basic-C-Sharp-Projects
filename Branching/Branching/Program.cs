0bdee89e05f8d460ffb33512c19933430dea7201
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-16 02:18:33 -0800

    ternery(?) operator
    makes branching more compact

--- Branching/Branching/Program.cs
+++ Branching/Branching/Program.cs
@@ -13,18 +13,20 @@ namespace Branching
             int currentTemp = 80;
             int roomTemp = 70;
 
-            if (currentTemp == roomTemp)
-            {
-                Console.WriteLine("yes it it room temp");
-            }
-            else if (currentTemp > roomTemp)
-            {
-                Console.WriteLine("it is warmer");
-            }
-            else
-            {
-                Console.WriteLine("not exactly room temp");
-            }
+            string compareResults = currentTemp == roomTemp ? "It is room temp" : "it is not room temp";
+
+            //if (currentTemp == roomTemp)
+            //{
+            //    Console.WriteLine("yes it it room temp");
+            //}
+            //else if (currentTemp > roomTemp)
+            //{
+            //    Console.WriteLine("it is warmer");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("not exactly room temp");
+            //}
         }
     }
 }
