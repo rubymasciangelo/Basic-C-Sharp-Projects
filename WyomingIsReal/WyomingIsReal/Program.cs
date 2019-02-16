f9aebe11be94e974559e925d72714707c23854e3
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-16 04:12:20 -0800

    while loop added

--- WyomingIsReal/WyomingIsReal/Program.cs
+++ WyomingIsReal/WyomingIsReal/Program.cs
@@ -23,14 +23,45 @@ namespace WyomingIsReal
                         bigLie = true;
                         break;
                     default:
-                        Console.WriteLine("Go bigger. Tell me a bigger lie.");
+                        Console.WriteLine("No, go bigger.");
+                        Console.WriteLine("Tell me a bigger lie:");
+                        lie = Console.ReadLine();
                         break;
                 }
             }
             while (!bigLie);
 
             Console.WriteLine("Name an American state");
+            string state = Console.ReadLine();
+            bool america = state != "Wyoming";
 
+            while (1=1)
+            {
+                switch (state)
+                {
+                    case "Florida":
+                        Console.WriteLine("Is Florida a part of America, though? Or is America... part of Florida...");
+                        Console.WriteLine("Tell me another American state:");
+                        state = Console.ReadLine();
+                        break;
+                    case "Wyoming":
+                        Console.WriteLine("Ha! The truth is out there and it's life-changing.");
+                        Console.WriteLine("Tell me an actual American state though:");
+                        state = Console.ReadLine();
+                        break;
+                    case "Iowa":
+                        Console.WriteLine("Perhaps, but Does It Spark Joy?");
+                        Console.WriteLine("Tell me another American state:");
+                        state = Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("Seems real! Tell me another American state:");
+                        state = Console.ReadLine();
+                        break;
+                }
+            }
+
+            Console.Read();
         }
     }
 }
