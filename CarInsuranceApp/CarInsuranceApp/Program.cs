e35c208ee65a18a05f62ec7e716ab919a9f85f65
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-16 01:26:10 -0800

    made dui question #1

--- CarInsuranceApp/CarInsuranceApp/Program.cs
+++ CarInsuranceApp/CarInsuranceApp/Program.cs
@@ -11,15 +11,18 @@ namespace CarInsuranceApp
         static void Main(string[] args)
         {
             Console.WriteLine("Car Insurance Application Survey:");
+            Console.WriteLine("Have you ever had a DUI? Please answer with either " + '"' + "true" + '"' + "or" + '"' + "false" + '"');
+            string duiResponse = Console.ReadLine();
+            bool duiStatus = Convert.ToBoolean(duiResponse);
+            Console.Read();
+
+
             Console.WriteLine("How old are you?");
             string ageResponse = Console.ReadLine();
             int age = Convert.ToInt16(ageResponse);
             Console.Read();
 
-            Console.WriteLine("Have you ever had a DUI? Please answer with either " + '"' + "true" + '"' + "or" + '"' + "false" + '"');
-            string duiResponse = Console.ReadLine();
-            bool duiStatus = Convert.ToBoolean(duiResponse);
-            Console.Read();
+            
 
             Console.WriteLine("How many speeding tickets do you have?");
             string ticketsResponse = Console.ReadLine();
