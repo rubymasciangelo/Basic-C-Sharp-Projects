1673e17a20a6c73d3203399f6e8166f42a455e33
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-16 03:48:37 -0800

    do while written

---
+++ WyomingIsReal/WyomingIsReal/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WyomingIsReal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tell me a lie:");
+            string lie = Console.ReadLine();
+            bool bigLie = lie == "Wyoming is real";
+
+            do
+            {
+                switch (lie)
+                {
+                    case "Wyoming is real":
+                        Console.WriteLine("You know too much. Run.");
+                        bigLie = true;
+                        break;
+                    default:
+                        Console.WriteLine("Go bigger. Tell me a bigger lie.");
+                        break;
+                }
+            }
+            while (!bigLie);
+
+            Console.WriteLine("Name an American state");
+
+        }
+    }
+}
