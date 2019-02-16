0dbe51f0c4391bd9a6884f77d5f6d63fe888b2ee
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-16 04:36:38 -0800

    escape sequences inside of strings

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jesse";
+            string quote = "The man said, \\Henlo\", Jesse. \n Hello on a new line. \n \t Hello on a tab.";
+            string fileName = @"C:\\Users\\Jesse";
+        }
+    }
+}
