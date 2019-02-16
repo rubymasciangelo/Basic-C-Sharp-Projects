0d1f4c1484dbb9b5eded75cf8865e663e0d99ff2
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-16 02:16:24 -0800

    branching in long form

---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentTemp = 80;
+            int roomTemp = 70;
+
+            if (currentTemp == roomTemp)
+            {
+                Console.WriteLine("yes it it room temp");
+            }
+            else if (currentTemp > roomTemp)
+            {
+                Console.WriteLine("it is warmer");
+            }
+            else
+            {
+                Console.WriteLine("not exactly room temp");
+            }
+        }
+    }
+}
