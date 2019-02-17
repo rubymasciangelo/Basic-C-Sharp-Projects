de9be252b8f487a2584ae4a3db006da97d1fd39e
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-16 18:05:09 -0800

    attempting stringbbuilder

---
+++ CountryRoad/CountryRoad/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountryRoad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder bestSong = new StringBuilder();
+
+            bestSong.Append("Country Road");
+            bestSong.Append("Take me home");
+            bestSong.Append("To the place");
+            bestSong.Append("I belong");
+
+            Console.WriteLine(bestSong);
+            Console.Read();
+        }
+    }
+}
