14de505ebffbc1862084cffffaea148a32beb68a
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-16 19:24:54 -0800

    basic number arrays

--- ArraysAndLists/ArraysAndLists/Program.cs
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -6,7 +6,16 @@ using System.Collections.Generic;
     {
         static void Main()
         {
-            
+        int[] numArray = new int[5];
+        numArray[0] = 5;
+        numArray[1] = 2;
+        numArray[2] = 10;
+        numArray[3] = 200;
+        numArray[4] = 5000;
+
+        int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
+        //same thing but cleaner
+   
         }
     }
 
