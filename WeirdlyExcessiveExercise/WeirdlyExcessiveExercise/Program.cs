037d8546db60c3a89f38863d469db531540cf8d6
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-16 23:56:58 -0800

    step 7 edit

--- WeirdlyExcessiveExercise/WeirdlyExcessiveExercise/Program.cs
+++ WeirdlyExcessiveExercise/WeirdlyExcessiveExercise/Program.cs
@@ -146,21 +146,22 @@ namespace WeirdlyExcessiveExercise
 
             foreach (string item in items)
             {
-                if (guess != item)
-                {
-                    Console.WriteLine("That wasn't even on my list! Try again!");
-                    guess = Console.ReadLine();
-                    index = items.IndexOf(guess);
-                }
-                else if (guess != "sunscreen")
+                
+                if (guess != "sunscreen" && guess == item)
                 {
                     Console.WriteLine("Nope! Guess again! On my list, " + guess + " is number " + index);
                     guess = Console.ReadLine();
                     index = items.IndexOf(guess);
                 }
-                else
+                else if (guess == "sunscreen")
                 {
                     Console.WriteLine("Yes! the sun is the most important enemy!");
+                }
+                else
+                {
+                    Console.WriteLine("That wasn't even on my list! Try again!");
+                    guess = Console.ReadLine();
+                    index = items.IndexOf(guess);
                 }
             }
             Console.Read();
