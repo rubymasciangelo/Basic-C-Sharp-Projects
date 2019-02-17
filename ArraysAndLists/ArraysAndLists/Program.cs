44035747cd882a0746352a29fae30f2597bfc79b
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-16 19:34:38 -0800

    lists

--- ArraysAndLists/ArraysAndLists/Program.cs
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -14,7 +14,20 @@ using System.Collections.Generic;
         numArray[4] = 5000;
 
         int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
-        //same thing but cleaner
+        //same thing but cleaner and you don't have to set the length
+
+        int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+        //it can guess that you want to make an int array
+
+        numArray2[5] = 650;
+        //changing value
+
+        List<int> intList = new List<int>();
+        intList.Add(4);
+        intList.Add(10);
+        intList.Remove(10);
+
+        Console.WriteLine(intList[0]);
    
         }
     }
