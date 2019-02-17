851fb287d50e194086766fe43911820311ef4219
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-16 22:30:11 -0800

    step 2 added

--- WeirdlyExcessiveExercise/WeirdlyExcessiveExercise/Program.cs
+++ WeirdlyExcessiveExercise/WeirdlyExcessiveExercise/Program.cs
@@ -10,25 +10,43 @@ namespace WeirdlyExcessiveExercise
             //1. Create 1D string[], ask user to input text, Create a loop that goes through eachstring in the array
             //   adding the user's text to the string. Then create a loop that prints each string on a separate line
 
-            Console.WriteLine("Type any singular noun:");
-            string noun1 = Console.ReadLine();
+            //Console.WriteLine("Type any singular noun:");
+            //string noun1 = Console.ReadLine();
 
-            Console.WriteLine("Type any plural noun:");
-            string noun2 = Console.ReadLine();
+            //Console.WriteLine("Type any plural noun:");
+            //string noun2 = Console.ReadLine();
 
-            Console.WriteLine("Type any amount of time:");
-            string time1 = Console.ReadLine();
+            //Console.WriteLine("Type any amount of time:");
+            //string time1 = Console.ReadLine();
 
-            Console.WriteLine("Type any abstract concept:");
-            string concept1 = Console.ReadLine();
+            //Console.WriteLine("Type any abstract concept:");
+            //string concept1 = Console.ReadLine();
 
-            string[] wannabe = { "If you wanna be my " + noun1, "You gotta get with my " + noun2, "Make it last " + time1, "Cause " + concept1 + " never ends!", };
+            //string[] wannabe = { "If you wanna be my " + noun1, "You gotta get with my " + noun2, "Make it last " + time1, "Cause " + concept1 + " never ends!", };
 
-            for (int j = 0; j < wannabe.Length; j++)
+            //for (int j = 0; j < wannabe.Length; j++)
+            //{
+            //    Console.WriteLine(wannabe[j]);
+            //}
+            //Console.ReadLine();
+
+            //2. Create an infinite loop
+
+            Console.WriteLine("Insult me:");
+            string insult = Console.ReadLine();
+
+            while (true)
             {
-                Console.WriteLine(wannabe[j]);
+                switch (insult)
+                {
+                    default:
+                        Console.WriteLine("I'm rubber, you're glue! Whatever you say bounces off me and sticks to you!");
+                        Console.WriteLine("Insult me:");
+                        insult = Console.ReadLine();
+                        break;
+                }
+                //Console.ReadLine();
             }
-            Console.ReadLine();
         }
     }
 }
