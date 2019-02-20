b1c5cf2dc72450c9d43f49896304ce998457ed94
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-19 21:21:02 -0800

    exception added

--- ExceptionHandling/ExceptionHandling/Program.cs
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -4,19 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExceptionHandling
-    {
+
         class Program
         {
             static void Main(string[] args)
             {
-                Console.WriteLine("Pick a number");
-                int number = Convert.ToInt16(Console.ReadLine());
-                Console.WriteLine("Pick a second number");
-                int numberTwo = Convert.ToInt16(Console.ReadLine());
-                Console.WriteLine("Dividing the two...");
-                int numberThree = number / numberTwo;
-                Console.WriteLine(number + " divided by " + numberTwo + " = " + numberThree);
+        try
+        {
+            Console.WriteLine("Pick a number");
+            int number = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Pick a second number");
+            int numberTwo = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Dividing the two...");
+            int numberThree = number / numberTwo;
+            Console.WriteLine(number + " divided by " + numberTwo + " = " + numberThree);
+            Console.ReadLine();
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.ReadLine();
+        }
+         
+            
             }
         }
-    }
