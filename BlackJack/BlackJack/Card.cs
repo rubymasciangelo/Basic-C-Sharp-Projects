bd3a254056b634b738188bdccffd4900c43d9266
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-19 23:59:08 -0800

    created a constructor

--- BlackJack/BlackJack/Card.cs
+++ BlackJack/BlackJack/Card.cs
@@ -8,6 +8,11 @@ namespace BlackJack
 {
     public class Card
     {
+        public Card()
+        {
+            Suit = "Spades";
+            Face = "Two";
+        }
         public string Suit { get; set; }
         public string Face { get; set; }
     }
--- BlackJack/BlackJack/Program.cs
+++ BlackJack/BlackJack/Program.cs
@@ -11,8 +11,8 @@ namespace BlackJack
         static void Main(string[] args)
         {
             Card cardOne = new Card();
-            cardOne.Face = "Queen";
-            cardOne.Suit = "Spades";
+            //cardOne.Face = "Queen";
+            //cardOne.Suit = "Spades";
 
             Console.WriteLine(cardOne.Face + " of " + cardOne.Suit);
                 Console.ReadLine();
