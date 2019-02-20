8b0d9a7626b35381154ea0f37074389eab3a0009
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-19 22:00:09 -0800

    program created

---
+++ BigDivision/BigDivision/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 4, 8, 15, 16, 23, 32 };
+            Console.WriteLine("Choose a number to divide a list of numbers by:");
+            int numChoice = Convert.ToInt16(Console.ReadLine());
+            foreach (int number in numbers)
+            {
+                int numAnswer = number / numChoice;
+                Console.WriteLine(number + " divided by " + numChoice + " = " + numAnswer);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
