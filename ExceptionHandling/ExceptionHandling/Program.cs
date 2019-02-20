7b007d47c24f150053bf23cce9faa4d96bc82b7c
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-19 21:11:52 -0800

    pick a number added

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Pick a number");
+                int number = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("Pick a second number");
+                int numberTwo = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("Dividing the two...");
+                int numberThree = number / numberTwo;
+                Console.WriteLine(number + " divided by " + numberTwo + " = " + numberThree);
+            }
+        }
+    }
