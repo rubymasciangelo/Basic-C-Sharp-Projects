e821e9a5e37621df87ee0b23e0d041284a94794f
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-19 23:54:29 -0800

    properties suit and face added with get and set

---
+++ BlackJack/BlackJack/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    public class Card
+    {
+        public string Suit { get; set; }
+        public string Face { get; set; }
+    }
+}
