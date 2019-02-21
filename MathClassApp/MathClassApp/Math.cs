78484582bf4ca90ffc981720195ae434f8e043e6
rubymasciangelo <72hermitcrabs@gmail.com>
2019-02-21 01:50:24 -0800

    first method added

---
+++ MathClassApp/MathClassApp/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClassApp
+{
+    public class Math
+    {
+        public int Multiply(int inputNum)
+        {
+            int times3 = inputNum * 3;
+            Console.WriteLine(inputNum + " multiplied by 3 is " + times3 );
+        }
+    }
+}
---
+++ MathClassApp/MathClassApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClassApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose an integer to do some math to:");
+            Int16 inputNum = Convert.ToInt16("Console.Readline");
+        }
+    }
+}
