8db21b7eeb2ed4ba5e445ffa637254a7edc68b2d
rubymasciangelo <72hermitcrabs@gmail.com>
2019-03-25 19:50:27 -0700

    switched back to method overload

--- MuchMath/MuchMath/Okurrr.cs
+++ MuchMath/MuchMath/Okurrr.cs
@@ -8,14 +8,13 @@ namespace MuchMath
 {
     public class Okurrr
     {
-        public static int SomeMath(string input1, string input2 = "0")
+        public static int SomeMath(string input1)
+        {
+            return Convert.ToInt16(input1) + 3;
+        }
+        public static int SomeMath(string input1, string input2)
         {
             return Convert.ToInt16(input1) + Convert.ToInt16(input2) + 3;
         }
-        //public static int SomeMath(int input1, int input2)
-        //{
-        //    int plus3 = Convert.ToInt16
-        //    return input1 + input2 + 3;
-        //}
     }
 }
