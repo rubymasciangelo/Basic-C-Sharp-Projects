36c7cf121fc7e0583ad1162c0d985c6a39814110
rubymasciangelo <72hermitcrabs@gmail.com>
2019-03-25 18:29:07 -0700

    SomeMath added

---
+++ MuchMath/MuchMath/Okurrr.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuchMath
+{
+    public class Okurrr
+    {
+        public static int SomeMath(int input1, int input2 = 0)
+        {
+            int plus3 = input1 + input2 + 3;
+            return plus3;
+        }
+    }
+}
