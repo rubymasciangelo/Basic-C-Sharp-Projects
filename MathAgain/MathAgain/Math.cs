055f4babf5e90df5d8564e86d6e27521a80a0ab9
rubymasciangelo <72hermitcrabs@gmail.com>
2019-03-25 17:27:39 -0700

    method add2 added

---
+++ MathAgain/MathAgain/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAgain
+{
+    public class Math
+    {
+        public static int Add2(int inputX)
+        {
+            int plus2 = inputX + 2;
+            return plus2;
+        }
+    }
+}
