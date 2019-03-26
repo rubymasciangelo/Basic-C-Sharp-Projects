558dd7bbc283199da73319bf63ec9b435efffd42
rubymasciangelo <72hermitcrabs@gmail.com>
2019-03-25 20:02:27 -0700

    if else statement added to main

--- MuchMath/MuchMath/Program.cs
+++ MuchMath/MuchMath/Program.cs
@@ -14,7 +14,14 @@ namespace MuchMath
             string input1 = Console.ReadLine();
             Console.WriteLine("You totally don't have to but you may add a second number: ");
             string input2 = Console.ReadLine();
-            Console.WriteLine(Okurrr.SomeMath(input1, input2));
+            if (input2 != null)
+            {
+                Console.WriteLine(Okurrr.SomeMath(input1, input2));
+            }
+            else
+            {
+                Console.WriteLine(Okurrr.SomeMath(input1));
+            }
             Console.ReadKey();
         }
     }
