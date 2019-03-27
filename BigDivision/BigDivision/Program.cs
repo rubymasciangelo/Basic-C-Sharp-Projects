57b16aab68c8afc19b653281fb6caae36b749714
rubymasciangelo <72hermitcrabs@gmail.com>
2019-03-27 12:26:31 -0700

    variable declaration and conversion for number choice moved out of try catch loop

--- BigDivision/BigDivision/Program.cs
+++ BigDivision/BigDivision/Program.cs
@@ -13,18 +13,19 @@ namespace BigDivision
             
                 List<int> numbers = new List<int>() { 4, 8, 15, 16, 23, 32 };
                 Console.WriteLine("Choose a number to divide a list of numbers by:");
+                int numChoice = Convert.ToInt16(Console.ReadLine());
 
-                //Console.ReadLine();
-
-                try
+            try
                 {
                 foreach (int number in numbers)
                 {
-                    int numChoice = Convert.ToInt16(Console.ReadLine());
+                    
                     int numAnswer = number / numChoice;
                     Console.WriteLine(number + " divided by " + numChoice + " = " + numAnswer);
+                    
                 }
-                }
+                
+            }
                 catch (FormatException ex)
                 {
                     Console.WriteLine(ex.Message);
