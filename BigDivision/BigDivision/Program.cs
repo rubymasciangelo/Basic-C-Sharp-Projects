40e221fda8b15a8d60ca2221c2a57fa9034773da
rubymasciangelo <72hermitcrabs@gmail.com>
2019-03-28 16:05:35 -0700

    final fix

--- BigDivision/BigDivision/Program.cs
+++ BigDivision/BigDivision/Program.cs
@@ -12,25 +12,24 @@ namespace BigDivision
         {
             
                 List<int> numbers = new List<int>() { 4, 8, 15, 16, 23, 32 };
+                Console.WriteLine("Choose a number to divide a list of numbers by:");
                 
-
 
 
             try
             {
-                Console.WriteLine("Choose a number to divide a list of numbers by:");
                 string numChoice = Console.ReadLine();
                 int numChoice2 = Convert.ToInt16(numChoice);
                 if (numChoice2 == 0)
                 {
-                    throw new DivideByZeroException();
+                    throw new DivideByZeroException("Cannot divide by 0");
                 }
                 foreach (int number in numbers)
                 {
 
                     int numAnswer = number / Convert.ToInt16(numChoice);
                     Console.WriteLine(number + " divided by " + numChoice + " = " + numAnswer);
-
+                    
 
                 }
             }
