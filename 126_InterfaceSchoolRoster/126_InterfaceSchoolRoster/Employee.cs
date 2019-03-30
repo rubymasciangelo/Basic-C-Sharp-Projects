a2bd05b78323982fb0305a653fd1d664ea08d367
rubymasciangelo <72hermitcrabs@gmail.com>
2019-03-29 19:41:46 -0700

    program created and previous program added

---
+++ 126_InterfaceSchoolRoster/126_InterfaceSchoolRoster/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _126_InterfaceSchoolRoster
+{
+    class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ 126_InterfaceSchoolRoster/126_InterfaceSchoolRoster/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _126_InterfaceSchoolRoster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            employee1.FirstName = "Sample";
+            employee1.LastName = "Student";
+
+            employee1.SayName();
+            Console.ReadKey();
+        }
+    }
+}
