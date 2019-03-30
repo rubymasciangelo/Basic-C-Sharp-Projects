dc12736e91661073761a6146052124e41cd42ff8
rubymasciangelo <72hermitcrabs@gmail.com>
2019-03-29 18:19:49 -0700

    employee object instantiated and sayname called

--- 124_AbstractSchoolRoster/124_AbstractSchoolRoster/Program.cs
+++ 124_AbstractSchoolRoster/124_AbstractSchoolRoster/Program.cs
@@ -10,6 +10,12 @@ namespace _124_AbstractSchoolRoster
     {
         static void Main(string[] args)
         {
+            Employee employee1 = new Employee();
+            employee1.FirstName = "Sample";
+            employee1.LastName = "Student";
+
+            employee1.SayName();
+            Console.ReadKey();
         }
     }
 }
