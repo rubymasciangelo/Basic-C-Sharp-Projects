fd72bc31e1ccbc95a8ddd8322b2e361576e32e03
rubymasciangelo <72hermitcrabs@gmail.com>
2019-04-02 19:09:05 -0700

    comments added

---
+++ 131_SchoolRosterGenerics/131_SchoolRosterGenerics/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _131_SchoolRosterGenerics
+{
+    class Employee<T> : Person
+    {
+        public List<T> things { get; set; }
+
+     
+    public void Print() //method to print all variable type items 'thing' in generic list 'things'
+        {
+            foreach (var thing in things)
+            {
+                Console.WriteLine(thing);
+            }
+        }
+    }
+}
---
+++ 131_SchoolRosterGenerics/131_SchoolRosterGenerics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _131_SchoolRosterGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> stringList = new Employee<string>();
+            stringList.things = new List<string> { "teacher", "custodian", "bus driver", "counselor"};
+
+            Employee<int> intList = new Employee<int>();
+            intList.things = new List<int> { 1, 2, 3, 4 };
+
+
+            stringList.Print();
+            intList.Print(); //calling stringList ans intList to print all items to console
+
+
+            Console.ReadKey();
+        }
+    }
+}
