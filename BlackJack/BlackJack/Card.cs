d550694d7b65200c3d9332ea661e247efcde7b3e
rubymasciangelo <72hermitcrabs@gmail.com>
2019-04-02 20:11:16 -0700

    enums added for card building

--- BlackJack/BlackJack/Card.cs
+++ BlackJack/BlackJack/Card.cs
@@ -8,12 +8,30 @@ namespace BlackJack
 {
     public class Card
     {
-        public Card()
-        {
-            Suit = "Spades";
-            Face = "Two";
-        }
-        public string Suit { get; set; }
-        public string Face { get; set; }
+        public Suit Suit { get; set; }
+        public Face Face { get; set; }
+    }
+    public enum Suit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+    public enum Face
+    {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
     }
 }
--- BlackJack/BlackJack/Deck.cs
+++ BlackJack/BlackJack/Deck.cs
@@ -11,27 +11,21 @@ namespace BlackJack
         public Deck()
         {
             Cards = new List<Card>();
-            List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" };
-            List<string> Faces = new List<string>()
+            
+            for (int i = 0; i < 13; i++)
             {
-                "Two", "Three", "Four", "Five", "Six", "Seven", "EIght",
-                "Nine", "Ten", "Jack", "Queen", "King", "Ace"
-            };
-
-            foreach (string face in Faces)
-            {
-                foreach (string suit in Suits)
+                for (int j = 0; j < 4; j++)
                 {
                     Card card = new Card();
-                    card.Suit = suit;
-                    card.Face = face;
+                    card.Face = (Face)i;
+                    card.Suit = (Suit)j;
                     Cards.Add(card);
                 }
             }
         }
         public List<Card> Cards { get; set; }
 
-        public Deck Shuffle(int times = 1)
+        public void Shuffle(int times = 1)
         {
             for (int i = 0; i < times; i++)
             { 
