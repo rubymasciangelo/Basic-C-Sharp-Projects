7d9e7dbde87636f8cae434bb3d3a2830ed74606a
rubymasciangelo <72hermitcrabs@gmail.com>
2019-04-02 21:05:24 -0700

    code commented

---
+++ 134_EnumsOfTheWeek/134_EnumsOfTheWeek/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _134_EnumsOfTheWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("What day is it today?");
+                string today = Console.ReadLine();
+                Days day = (Days)Enum.Parse(typeof(Days), today, true); //coverts input string to its corresponding enum value
+            }
+            catch(ArgumentException) //catches anything that isn't an enumeration
+            {
+                Console.WriteLine("Please write an actual day of the week");
+            }
+            Console.Read();
+        }
+
+        public enum Days
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
