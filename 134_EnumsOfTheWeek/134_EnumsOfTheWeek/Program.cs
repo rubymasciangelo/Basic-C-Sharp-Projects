5803812b42c48a7f149c44f33b5e8b7c9df5c808
rubymasciangelo <72hermitcrabs@gmail.com>
2019-04-09 19:33:22 -0700

    try parse for int input added

--- 134_EnumsOfTheWeek/134_EnumsOfTheWeek/Program.cs
+++ 134_EnumsOfTheWeek/134_EnumsOfTheWeek/Program.cs
@@ -12,38 +12,74 @@ namespace _134_EnumsOfTheWeek
         {
             //Console.WriteLine("What day is it today?");
             //string today = Console.ReadLine();
-            
+
+
+            //    try
+            //    {
+            //        string[] dayStrings = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            //        Console.WriteLine("What day is it today?");
+            //        string today = Console.ReadLine();
+            //        Days day = (Days)Enum.Parse(typeof(Days), today, true); //coverts input string to its corresponding enum value
+
+            //        foreach (string dayString in dayStrings)
+            //        {
+            //            try
+            //            {
+            //                Days dayValue = (Days)Enum.Parse(typeof(Days), dayString);
+
+            //                if //(today == dayString)//(Enum.IsDefined(typeof(Days), dayValue))
+            //                {
+            //                    Console.WriteLine("You think today is " + today + "?");
+            //                }
+            //                else
+            //                {
+            //                    Console.WriteLine("That's not an actual day of the week");
+            //                }
+            //            }
+            //            finally
+            //            {
+            //                Console.WriteLine("Maybe it is!");
+            //            }
+            //            break;
+            //        }
+
+
+            //    }
+            //    catch (ArgumentException) //catches anything that isn't an enumeration
+            //    {
+            //        Console.WriteLine("That's not an actual day of the week");
+            //    }
+            //    Console.Read();
+            //}
+
+            //public enum Days
+            //{
+            //    Monday,
+            //    Tuesday,
+            //    Wednesday,
+            //    Thursday,
+            //    Friday,
+            //    Saturday,
+            //    Sunday
+            //}
 
             try
             {
-                string[] dayStrings = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
                 Console.WriteLine("What day is it today?");
                 string today = Console.ReadLine();
                 Days day = (Days)Enum.Parse(typeof(Days), today, true); //coverts input string to its corresponding enum value
                 
-                foreach (string dayString in dayStrings)
+                int inputInt;
+                bool intException = Int32.TryParse(today, out inputInt); //uses a bool to check if a tryparse to convert string input to an integer would be true
+                if (intException) //catches that potential integer input and returns an error message.
                 {
-                    try
-                    {
-                        Days dayValue = (Days)Enum.Parse(typeof(Days), dayString);
-
-                        if //(today == dayString)//(Enum.IsDefined(typeof(Days), dayValue))
-                        {
-                            Console.WriteLine("You think today is " + today + "?");
-                        }
-                        else
-                        {
-                            Console.WriteLine("That's not an actual day of the week");
-                        }
-                    }
-                    finally
-                    {
-                        Console.WriteLine("Maybe it is!");
-                    }
-                    break;
+                    Console.WriteLine("That's not an actual day of the week");
                 }
-                
-               
+                else
+                {
+                    Console.WriteLine("You think today is " + today + "?");
+                    Console.WriteLine("Maybe it is!");
+                }
             }
             catch (ArgumentException) //catches anything that isn't an enumeration
             {
