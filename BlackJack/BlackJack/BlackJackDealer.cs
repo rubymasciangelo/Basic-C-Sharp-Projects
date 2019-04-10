8c2e140684da3bf9def6d2e9484db97c74c61386
rubymasciangelo <72hermitcrabs@gmail.com>
2019-04-09 22:09:55 -0700

    many things added forgot about commiting as I go

---
+++ BlackJack/BlackJack/BlackJackDealer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    public class BlackJackDealer : Dealer
+    {
+        public List<Card> Hand { get; set; }
+        public bool Stay { get; set; }
+        public bool isBusted { get; set; }
+    }
+}
---
+++ BlackJack/BlackJack/BlackJackRules.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    public class BlackJackRules
+    {
+        private static Dictionary<Face, int> _cardValues = new Dictionary<Face, int>()
+        {
+            [Face.Two] = 2,
+            [Face.Three] = 3,
+            [Face.Four] = 4,
+            [Face.Five] = 5,
+            [Face.Six] = 6,
+            [Face.Seven] = 7,
+            [Face.Eight] = 8,
+            [Face.Nine] = 9,
+            [Face.Ten] = 10,
+            [Face.Jack] = 10,
+            [Face.Queen] = 10,
+            [Face.King] = 10,
+            [Face.Ace] = 1
+        };
+
+        public static int[] GetAllPossibleHandValues(List<Card> Hand)
+        {
+            int aceCount = Hand.Count(x => x.Face == Face.Ace);
+            int[] result = new int[aceCount + 1];
+            int value = Hand.Sum(x => _cardValues[x.Face]);
+            result[0] = value;
+            if (result.Length == 1) return result;
+        }
+
+        public static bool CheckForBlackJack(List<Card> Hand)
+        {
+
+        }
+    }
+}
