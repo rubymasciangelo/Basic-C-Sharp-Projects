c1b5da4e25828ece6c26ec277a2dc3706b1f1f81
rubymasciangelo <72hermitcrabs@gmail.com>
2019-04-09 19:45:54 -0700

    previous attempt at catching ints deleted

--- 134_EnumsOfTheWeek/134_EnumsOfTheWeek/Program.cs
+++ 134_EnumsOfTheWeek/134_EnumsOfTheWeek/Program.cs
@@ -10,58 +10,6 @@ namespace _134_EnumsOfTheWeek
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("What day is it today?");
-            //string today = Console.ReadLine();
-
-
-            //    try
-            //    {
-            //        string[] dayStrings = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
-            //        Console.WriteLine("What day is it today?");
-            //        string today = Console.ReadLine();
-            //        Days day = (Days)Enum.Parse(typeof(Days), today, true); //coverts input string to its corresponding enum value
-
-            //        foreach (string dayString in dayStrings)
-            //        {
-            //            try
-            //            {
-            //                Days dayValue = (Days)Enum.Parse(typeof(Days), dayString);
-
-            //                if //(today == dayString)//(Enum.IsDefined(typeof(Days), dayValue))
-            //                {
-            //                    Console.WriteLine("You think today is " + today + "?");
-            //                }
-            //                else
-            //                {
-            //                    Console.WriteLine("That's not an actual day of the week");
-            //                }
-            //            }
-            //            finally
-            //            {
-            //                Console.WriteLine("Maybe it is!");
-            //            }
-            //            break;
-            //        }
-
-
-            //    }
-            //    catch (ArgumentException) //catches anything that isn't an enumeration
-            //    {
-            //        Console.WriteLine("That's not an actual day of the week");
-            //    }
-            //    Console.Read();
-            //}
-
-            //public enum Days
-            //{
-            //    Monday,
-            //    Tuesday,
-            //    Wednesday,
-            //    Thursday,
-            //    Friday,
-            //    Saturday,
-            //    Sunday
-            //}
 
             try
             {
