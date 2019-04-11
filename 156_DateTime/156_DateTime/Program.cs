5d80b723357dcf8485c2a3e5b4d2f25ba2b09d15
rubymasciangelo <72hermitcrabs@gmail.com>
2019-04-11 04:58:38 -0700

    all added

---
+++ 156_DateTime/156_DateTime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _156_DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please input an integer:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("In {0} hours it will be " + DateTime.Now.AddHours(input), input);
+            //adds the integer input in hours to the current dateTime and writes to console
+            Console.Read();
+        }
+    }
+}
