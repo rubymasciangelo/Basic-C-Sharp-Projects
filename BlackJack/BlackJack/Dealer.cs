a26686aeee3af78dda538dbb0d313e7a65ae4ad3
rubymasciangelo <72hermitcrabs@gmail.com>
2019-04-11 04:19:22 -0700

    logging added

--- BlackJack/BlackJack/Dealer.cs
+++ BlackJack/BlackJack/Dealer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace BlackJack
 {
@@ -15,8 +16,13 @@ namespace BlackJack
         public void Deal(List<Card> Hand)
         {
             Hand.Add(Deck.Cards.First());
-            Console.WriteLine(Deck.Cards.First().ToString() + "\n");
-            Deck.Cards.RemoveAt(0);
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+            Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Student\Desktop\Basic_CS_Projects\Basic-C-Sharp-Projects\BlackJack\log.txt", true))
+            {
+                file.WriteLine(card);
+            }
+                Deck.Cards.RemoveAt(0);
         }
     }
 }
