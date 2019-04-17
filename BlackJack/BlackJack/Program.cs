df4d722304df1fa963105e08fc7e802a5c954d86
rubymasciangelo <72hermitcrabs@gmail.com>
2019-04-16 19:07:06 -0700

    part 1 of exception handling

--- BlackJack/BlackJack/Program.cs
+++ BlackJack/BlackJack/Program.cs
@@ -17,8 +17,16 @@ namespace BlackJackC
 
             Console.WriteLine("Welcome to the {0}. Let's start by telling me your name.", casinoName);
             string playerName = Console.ReadLine();
-            Console.WriteLine("How much money did you bring today?");
-            int bank = Convert.ToInt32(Console.ReadLine());
+
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("How much money did you bring today?");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals.");
+            }
+
             Console.WriteLine("Hello, {0}. Would you like to join a game of Black Jack right now?", playerName);
             string answer = Console.ReadLine().ToLower();
 
--- BlackJack/Casino/BlackJackGame.cs
+++ BlackJack/Casino/BlackJackGame.cs
@@ -22,11 +22,17 @@ namespace Casino.BlackJack
             Dealer.Stay = false;
             Dealer.Deck = new Deck();
             Dealer.Deck.Shuffle();
-            Console.WriteLine("Place your bet!");
 
             foreach (Player player in Players)
             {
-                int bet = Convert.ToInt32(Console.ReadLine());
+                bool validAnswer = false;
+                int bet = 0;
+                while (!validAnswer)
+                {
+                    Console.WriteLine("Place your bet!");
+                    validAnswer = int.TryParse(Console.ReadLine(), out bet);
+                    if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals");
+                }
                 bool successfullyBet = player.Bet(bet);
                 if (!successfullyBet)
                 {
