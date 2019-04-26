3680406e970b6a08debd402501e7ce7effca6457
rubymasciangelo <72hermitcrabs@gmail.com>
2019-04-26 14:24:19 -0700

    test

---
+++ VroomCarInsurance/VroomCarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VroomCarInsurance.Models;
+using VroomCarInsurance.ViewModels;
+
+namespace VroomCarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        private CarInsuranceEntities db = new CarInsuranceEntities();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+                var applicants = db.Applicants.ToList();
+                var applicantVms = new List<InsuranceVm>();
+                foreach (var applicant in applicants)
+                {
+                var applicantVm = new InsuranceVm
+                {
+                    Quote = applicant.Quote,
+                    FirstName = applicant.FirstName,
+                    LastName = applicant.LastName,
+                    EmailAddress = applicant.EmailAddress
+                };
+                applicantVms.Add(applicantVm);
+                }
+                return View(applicantVms);
+        }
+
+        // GET: Admin/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Applicant applicant = db.Applicants.Find(id);
+            if (applicant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicant);
+        }
+
+        // GET: Admin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Applicant applicant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Applicants.Add(applicant);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(applicant);
+        }
+
+        // GET: Admin/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Applicant applicant = db.Applicants.Find(id);
+            if (applicant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicant);
+        }
+
+        // POST: Admin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Applicant applicant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(applicant).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(applicant);
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Applicant applicant = db.Applicants.Find(id);
+            if (applicant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicant);
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Applicant applicant = db.Applicants.Find(id);
+            db.Applicants.Remove(applicant);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
